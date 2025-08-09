bb43a9262bf24f822429635f515dd926929aef09
Holo Nine <marius.paduraru@gmail.com>
2025-08-09 13:59:00 +0100

    First commit

---
+++ wp-data-bridge.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Plugin Name:       Wp Data Bridge
+ * Description:       Example block scaffolded with Create Block tool.
+ * Version:           0.0.1
+ * Requires at least: 6.7
+ * Requires PHP:      7.4
+ * Author:            The WordPress Contributors
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wp-data-bridge
+ *
+ * @package WpDataBridge
+ */
+
+if (! defined('ABSPATH')) {
+	exit; // Exit if accessed directly.
+}
